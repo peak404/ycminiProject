670c58a0b32fb9bf8186ec4b51099e5e4af9fcf6
sqi925 <sqi@Ss-MacBook-Pro.local>
2025-09-28 20:07:10 -0400

    add seeding for testing

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@
 
 const mongoose =require('mongoose')
 const CampGround = require("../models/campground");
+const {places,descriptors} = require('./seedHelpers')
 const cities = require('./cities')
 
 
@@ -17,16 +18,27 @@ async function main() {
 main().catch(err => console.log("something went wrong",err) );
 
 
+// const sample1 =array=>array[Math.floor(Math.random()*array.length)]
+const sample =function(array)
+{
+    if(!Array.isArray(array) || array.length=== 0)
+    {
+        return "None"
+    }
+    return array[Math.floor(Math.random()*array.length)]
+
+}
+
 const seedDB = async()=>
 {
     await CampGround.deleteMany({});
     // const c = new CampGround({title:"长乐"})
     for (let i =0;i<50;i++)
     {
-        const random1000 =Math.floor(Math.random()*1000); 
+        const random1000 =Math.floor(Math.random()*cities.length); 
         const camp = new CampGround({
-            location:`${cities[random1000].city},
-            ${cities[random1000].state}`
+            location:`${cities[random1000].city}, ${cities[random1000].state}`,
+            title:`${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
     }
